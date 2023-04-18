167159b5f64c628bc7f008dcb379c7d970d6eedc
Umer Khan <umer.khan@usask.ca>
2023-04-18 17:50:19 -0600

    Switched code to an OOP approach

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-import tkinter as tk
 import customtkinter as ctk
 
-# Window setup
-ctk.set_appearance_mode("System")
-mainWindow = ctk.CTk()
-mainWindow.geometry("1024x768")
-mainWindow.title("Hotel Neighbourhood")
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
 
-# Hotel URL input
-urlLabel = ctk.CTkLabel(mainWindow, text="Enter a hotel URL from booking.com:")
-urlLabel.pack(padx=20, pady=20)
-urlInput = tk.StringVar()
-urlEntry = ctk.CTkEntry(mainWindow, width=350, height=40, textvariable=urlInput)
-urlEntry.pack()
+        # Root window settings
+        ctk.set_appearance_mode("System")
+        self.geometry("1024x768")
+        self.title("Hotel Neighbourhood")
+
+        # Hotel URL input
+        self.url_lbl = ctk.CTkLabel(self, text="Enter a hotel URL from booking.com:")
+        self.url_lbl.pack(padx=20, pady=20)
+        self.url_input = ctk.StringVar()
+        self.url_entry = ctk.CTkEntry(self, width=350, height=40, textvariable=self.url_input)
+        self.url_entry.pack()
 
 
-mainWindow.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
