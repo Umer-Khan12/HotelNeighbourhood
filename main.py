e0423037cee88fd434bf8ae545c464bf46671f3a
Umer Khan <umer.khan@usask.ca>
2023-04-18 18:40:24 -0600

    Added classes for the ChatGPT and Default modes. Created placeholder buttons for selection.

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 import customtkinter as ctk
 
+class DefaultFrame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.test_label = ctk.CTkLabel(self, text="This is the Default window")
+        self.test_label.pack(padx=20, pady=20)
+
+        # Hotel URL input
+        self.url_lbl = ctk.CTkLabel(self, text="Enter a hotel URL from booking.com:")
+        self.url_lbl.pack(padx=20, pady=20)
+        self.url_input = ctk.StringVar()
+        self.url_entry = ctk.CTkEntry(self, width=350, height=40, textvariable=self.url_input)
+        self.url_entry.pack()
+
+
+class ChatGPTFrame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.test_label = ctk.CTkLabel(self, text="This is the ChatGPT window")
+        self.test_label.pack(padx=20, pady=20)
+
+        # Hotel URL input
+        self.url_lbl = ctk.CTkLabel(self, text="Enter a hotel URL from booking.com:")
+        self.url_lbl.pack(padx=20, pady=20)
+        self.url_input = ctk.StringVar()
+        self.url_entry = ctk.CTkEntry(self, width=350, height=40, textvariable=self.url_input)
+        self.url_entry.pack()
+
+
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -8,13 +36,23 @@ class App(ctk.CTk):
         ctk.set_appearance_mode("System")
         self.geometry("1024x768")
         self.title("Hotel Neighbourhood")
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.rowconfigure(0, weight=1)
 
-        # Hotel URL input
-        self.url_lbl = ctk.CTkLabel(self, text="Enter a hotel URL from booking.com:")
-        self.url_lbl.pack(padx=20, pady=20)
-        self.url_input = ctk.StringVar()
-        self.url_entry = ctk.CTkEntry(self, width=350, height=40, textvariable=self.url_input)
-        self.url_entry.pack()
+        # Buttons to choose between Default or ChatGPT mode
+        self.default_btn = ctk.CTkButton(self, text="Default Mode", command=self.switch_frame_to_default())
+        self.default_btn.grid(row=0, column=0, padx=20, pady=20)
+        self.cgpt_btn = ctk.CTkButton(self, text="ChatGPT Mode", command=self.switch_frame_to_cgpt())
+        self.cgpt_btn.grid(row=0, column=1, padx=20, pady=20)
+
+    def switch_frame_to_default(self):
+        pass
+
+    def switch_frame_to_cgpt(self):
+        pass
+
+
 
 
 if __name__ == "__main__":
